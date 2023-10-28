f236505807cfbcd4d5b109501974c6d8173bd0c0
komu003 <komu.ph342@gmail.com>
2023-10-28 17:11:31 +0900

    Revert "add: openapi-generator-cliを追加"
    This reverts commit 7a9b0875ee84745d588dba1ca6f7d8ec0f98edc7.

--- Dockerfile.go
+++
@@ -1,7 +0,0 @@
-FROM golang:latest
-
-RUN apt-get update && apt-get install -y default-jre
-
-RUN wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/5.2.0/openapi-generator-cli-5.2.0.jar -O /usr/local/bin/openapi-generator-cli && chmod +x /usr/local/bin/openapi-generator-cli
-
-WORKDIR /app
