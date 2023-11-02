bf0aafa5fa86f2e1bf0712eb8733c678dc3dd329
komu003 <komu.ph342@gmail.com>
2023-11-03 02:22:22 +0900

    fix refresh_migration->reset_db

--- go/scripts/reset_db.sh
+++ go/scripts/reset_db.sh
@@ -2,3 +2,4 @@
 
 migrate -database 'mysql://root:rootpassword@tcp(mysql)/mydatabase?query' -path migrations drop -f
 migrate -database 'mysql://root:rootpassword@tcp(mysql)/mydatabase?query' -path migrations up
+go run cmd/seed/main.go
