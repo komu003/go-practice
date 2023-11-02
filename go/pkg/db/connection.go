73ca765ce3e8f8450bc8710f772a6baf29e4ba2b
komu003 <komu.ph342@gmail.com>
2023-11-03 02:36:15 +0900

    add: gormのSQL文を標準出力

--- go/pkg/db/connection.go
+++ go/pkg/db/connection.go
@@ -4,6 +4,10 @@ package db
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
 )
 
 // データベース接続情報の変数
@@ -13,10 +17,23 @@ var DB *gorm.DB
 func InitDatabase() error {
 	dsn := "root:rootpassword@tcp(mysql)/mydatabase?charset=utf8mb4&parseTime=True&loc=Local"
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+
 	if err != nil {
 		return err
 	}
+
 	DB = db
 
 	return nil
