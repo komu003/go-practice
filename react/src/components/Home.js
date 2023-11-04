b66d55623677db5bfc24196833b2f3ff0e4fb6c2
komu003 <komu.ph342@gmail.com>
2023-11-04 20:07:04 +0900

    add: LoadingIndicator

--- react/src/components/Home.js
+++ react/src/components/Home.js
@@ -1,34 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import LoadingIndicator from './LoadingIndicator';
 
 const Home = () => {
   const [usersCount, setUsersCount] = useState(0);
   const [micropostsCount, setMicropostsCount] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('http://localhost:8080/api/users/count')
-      .then(response => {
-        setUsersCount(response.data.count); 
-      })
-      .catch(error => {
-        console.error('ユーザー数の取得中にエラーが発生しました：', error);
-      });
-
-    axios.get('http://localhost:8080/api/microposts/count')
-      .then(response => {
-        setMicropostsCount(response.data.count);
-      })
-      .catch(error => {
-        console.error('マイクロポスト数の取得中にエラーが発生しました：', error);
-      });
+    Promise.all([
+      axios.get('http://localhost:8080/api/users/count'),
+      axios.get('http://localhost:8080/api/microposts/count')
+    ])
+    .then(([usersResponse, micropostsResponse]) => {
+      setUsersCount(usersResponse.data.count);
+      setMicropostsCount(micropostsResponse.data.count);
+    })
+    .catch(error => {
+      console.error('データの取得中にエラーが発生しました：', error);
+    })
+    .finally(() => {
+      setLoading(false);
+    });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>ユーザー数：{usersCount}</p>
-        <p>マイクロポスト数：{micropostsCount}</p>
+        <p>ユーザー数：{loading ? <LoadingIndicator /> : usersCount}</p>
+        <p>マイクロポスト数：{loading ? <LoadingIndicator /> : micropostsCount}</p>
         <nav>
           <ul>
             <li><Link to="/users">ユーザー一覧</Link></li>
