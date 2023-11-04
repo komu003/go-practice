f3e88700165d830eea13401e5b9096a9b314baba
komu003 <komu.ph342@gmail.com>
2023-11-04 18:55:30 +0900

    fix: Homeコンポーネントにcount + 動線を表示

--- react/src/components/Home.js
+++ react/src/components/Home.js
@@ -1,25 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const [message, setMessage] = useState('');
+  const [usersCount, setUsersCount] = useState(0);
+  const [micropostsCount, setMicropostsCount] = useState(0);
 
   useEffect(() => {
-    axios.get('http://localhost:8080/api/hello')
+    axios.get('http://localhost:8080/api/users/count')
       .then(response => {
-        setMessage(response.data.message); 
+        setUsersCount(response.data.count); 
       })
       .catch(error => {
-        console.error('There was an error fetching data:', error);
+        console.error('ユーザー数の取得中にエラーが発生しました：', error);
+      });
+
+    axios.get('http://localhost:8080/api/microposts/count')
+      .then(response => {
+        setMicropostsCount(response.data.count);
+      })
+      .catch(error => {
+        console.error('マイクロポスト数の取得中にエラーが発生しました：', error);
       });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {message}
-        </p>
+        <p>ユーザー数：{usersCount}</p>
+        <p>マイクロポスト数：{micropostsCount}</p>
+        <nav>
+          <ul>
+            <li><Link to="/users">ユーザー一覧</Link></li>
+            <li><Link to="/microposts">マイクロポスト一覧</Link></li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
