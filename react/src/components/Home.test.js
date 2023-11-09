e226c3c63d97689487c3e1453ce009b014a32577
komu003 <komu.ph342@gmail.com>
2023-11-09 19:57:59 +0900

    add: Home.test.jsとフェッチの正常系テスト

--- react/src/components/Home.test.js
+++ react/src/components/Home.test.js
@@ -1,21 +1,53 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import axios from 'axios';
 import Home from './Home';
 import { MemoryRouter } from 'react-router-dom';
+import { API_BASE_URL } from '../config';
+import { act } from 'react-dom/test-utils';
 
 jest.mock('axios');
 
 describe('Home コンポーネント', () => {
-  test('初期状態でローディングインジケーターが表示される', async () => {
-    axios.get.mockImplementation(() => new Promise(() => {}));
-    render(
-      <MemoryRouter>
-        <Home />
-      </MemoryRouter>
-    );
-    const loadingIndicators = screen.getAllByTestId('loading-indicator');
-    expect(loadingIndicators.length).toBeGreaterThan(0);
+  test('ユーザー数とマイクロポスト数が表示される', async () => {
+    jest.useFakeTimers();
+  
+    axios.get.mockImplementation(url => {
+      switch (url) {
+        case `${API_BASE_URL}/users/count`:
+          return Promise.resolve({ data: { count: 10 } });
+        case `${API_BASE_URL}/microposts/count`:
+          return Promise.resolve({ data: { count: 5 } });
+        default:
+          throw new Error('not found');
+      }
+    });
+  
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      );
+    });
+  
+    expect(screen.getAllByTestId('loading-indicator').length).toBeGreaterThan(0);
+  
+    await act(async () => {
+      jest.advanceTimersByTime(499);
+    });
+  
+    expect(screen.getAllByTestId('loading-indicator').length).toBeGreaterThan(0);
+  
+    await act(async () => {
+      jest.advanceTimersByTime(1);
+    });
+  
+    expect(screen.queryByTestId('loading-indicator')).toBeNull();
+    expect(screen.getByText('ユーザー数：10')).toBeInTheDocument();
+    expect(screen.getByText('マイクロポスト数：5')).toBeInTheDocument();
+  
+    jest.useRealTimers();
   });
 });
