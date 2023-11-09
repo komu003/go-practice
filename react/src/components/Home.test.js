7625cb5c0880f85a0e55d4d8513b213f790c12db
komu003 <komu.ph342@gmail.com>
2023-11-09 19:59:10 +0900

    add: Home.test.jsにnetwork error時の異常系テストを追加

--- react/src/components/Home.test.js
+++ react/src/components/Home.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import axios from 'axios';
 import Home from './Home';
@@ -48,6 +48,39 @@ describe('Home コンポーネント', () => {
     expect(screen.getByText('ユーザー数：10')).toBeInTheDocument();
     expect(screen.getByText('マイクロポスト数：5')).toBeInTheDocument();
   
+    jest.useRealTimers();
+  });
+
+  test('エラー時はエラーが表示される', async () => {
+    jest.useFakeTimers();
+  
+    axios.get.mockImplementation(() => Promise.reject(new Error('Network Error')));
+  
+    await act(async () => {
+      render(
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      );
+    });
+  
+    expect(screen.getAllByTestId('loading-indicator').length).toBeGreaterThan(0);
+  
+    await act(async () => {
+      jest.advanceTimersByTime(499);
+    });
+  
+    expect(screen.getAllByTestId('loading-indicator').length).toBeGreaterThan(0);
+  
+    await act(async () => {
+      jest.advanceTimersByTime(1);
+    });
+  
+    expect(screen.queryByTestId('loading-indicator')).toBeNull();
+    const userCountElement = screen.getByText('ユーザー数：').closest('p');
+    expect(within(userCountElement).getByText('Error: error')).toBeInTheDocument();
+    const micropostCountElement = screen.getByText('マイクロポスト数：').closest('p');
+    expect(within(micropostCountElement).getByText('Error: error')).toBeInTheDocument();
     jest.useRealTimers();
   });
 });
