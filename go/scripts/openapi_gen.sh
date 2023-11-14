a71f11872e49876528561e895fed020ee25ba26f
komu003 <komu.ph342@gmail.com>
2023-11-15 02:17:41 +0900

    fix: generate時、ライブラリを目的としたファイルのみ生成

--- go/scripts/openapi_gen.sh
+++ go/scripts/openapi_gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openapi-generator-cli generate -i /api-spec/openapi.yml -g go-server -o /app/gen
+openapi-generator-cli generate -i /api-spec/openapi.yml -g go-server -o /app/gen --additional-properties=outputAsLibrary=true
