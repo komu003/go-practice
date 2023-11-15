05a64d8e06f36f808e08dd3b97115550bd6e4d9c
komu003 <komu.ph342@gmail.com>
2023-11-16 05:28:16 +0900

    fmt: go fmt

--- go/cmd/server/main.go
+++ go/cmd/server/main.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-    "context"
-    "log"
-    "net/http"
+	"app/models"
+	"app/ogen"
+	"app/pkg/db"
+	"context"
 	"errors"
-    "app/ogen"
-    "app/pkg/db"
-    "app/models"
+	"log"
+	"net/http"
 )
 
-type goPracticeService struct {}
+type goPracticeService struct{}
 
 func (s *goPracticeService) APIMicropostsCountGet(ctx context.Context) (ogen.APIMicropostsCountGetRes, error) {
 	if db.DB == nil {
-        return nil, errors.New("データベース接続が利用できません")
-    }
+		return nil, errors.New("データベース接続が利用できません")
+	}
 	var count int64
-    if err := db.DB.Model(&models.Micropost{}).Count(&count).Error; err != nil {
-        return &ogen.APIMicropostsCountGetInternalServerError{}, err
-    }
-    return &ogen.CountResponse{Count: ogen.NewOptInt(int(count))}, nil
+	if err := db.DB.Model(&models.Micropost{}).Count(&count).Error; err != nil {
+		return &ogen.APIMicropostsCountGetInternalServerError{}, err
+	}
+	return &ogen.CountResponse{Count: ogen.NewOptInt(int(count))}, nil
 }
 
 func (s *goPracticeService) APIUsersCountGet(ctx context.Context) (ogen.APIUsersCountGetRes, error) {
 	if db.DB == nil {
-        return nil, errors.New("データベース接続が利用できません")
-    }
+		return nil, errors.New("データベース接続が利用できません")
+	}
 	var count int64
-    if err := db.DB.Model(&models.User{}).Count(&count).Error; err != nil {
-        return &ogen.APIUsersCountGetInternalServerError{}, err
-    }
-    return &ogen.CountResponse{Count: ogen.NewOptInt(int(count))}, nil
+	if err := db.DB.Model(&models.User{}).Count(&count).Error; err != nil {
+		return &ogen.APIUsersCountGetInternalServerError{}, err
+	}
+	return &ogen.CountResponse{Count: ogen.NewOptInt(int(count))}, nil
 }
 
 func main() {
@@ -39,12 +39,12 @@ func main() {
 		log.Fatalf("データベース接続エラー: %v", err)
 	}
 
-    srv := &goPracticeService{}
-    httpServer, err := ogen.NewServer(srv)
-    if err != nil {
-        log.Fatalf("Failed to create server: %v", err)
-    }
+	srv := &goPracticeService{}
+	httpServer, err := ogen.NewServer(srv)
+	if err != nil {
+		log.Fatalf("Failed to create server: %v", err)
+	}
 
-    log.Println("Server is running at http://localhost:8080")
-    log.Fatal(http.ListenAndServe(":8080", httpServer))
+	log.Println("Server is running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", httpServer))
 }
