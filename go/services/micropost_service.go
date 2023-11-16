16b1b19bb1b38c1c004b3f1eed0fa9ad5bf4c0af
komu003 <komu.ph342@gmail.com>
2023-11-17 01:51:27 +0900

    delete: 不要なエラーハンドリングを削除

--- go/services/micropost_service.go
+++ go/services/micropost_service.go
@@ -5,16 +5,12 @@ import (
 	"app/ogen"
 	"app/pkg/db"
 	"context"
-	"errors"
 	"fmt"
 )
 
 type MicropostService struct{}
 
 func (s *MicropostService) APIMicropostsCountGet(ctx context.Context) (ogen.APIMicropostsCountGetRes, error) {
-	if db.DB == nil {
-		return nil, errors.New("データベース接続が利用できません")
-	}
 	var count int64
 	if err := db.DB.WithContext(ctx).Model(&models.Micropost{}).Count(&count).Error; err != nil {
 		errMsg := fmt.Errorf("データベースからのマイクロポスト数の取得に失敗しました: %w", err)
--- go/services/user_service.go
+++ go/services/user_service.go
@@ -5,19 +5,16 @@ import (
 	"app/ogen"
 	"app/pkg/db"
 	"context"
-	"errors"
 	"fmt"
 )
 
 type UserService struct{}
 
 func (s *UserService) APIUsersCountGet(ctx context.Context) (ogen.APIUsersCountGetRes, error) {
-	if db.DB == nil {
-		return nil, errors.New("データベース接続が利用できません")
-	}
 	var count int64
 	if err := db.DB.WithContext(ctx).Model(&models.User{}).Count(&count).Error; err != nil {
-		return &ogen.APIUsersCountGetInternalServerError{}, fmt.Errorf("データベースからのユーザー数の取得に失敗しました: %w", err)
+		errMsg := fmt.Errorf("データベースからのユーザー数の取得に失敗しました: %w", err)
+		return &ogen.APIUsersCountGetInternalServerError{}, errMsg
 	}
 	return &ogen.CountResponse{Count: ogen.NewOptInt(int(count))}, nil
 }
