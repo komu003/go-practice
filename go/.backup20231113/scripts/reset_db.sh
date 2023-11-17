fb8ad4d1a7c2ae0667acca1110e0cd7a2498bf5a
komu003 <komu.ph342@gmail.com>
2023-11-17 19:13:33 +0900

    delete: バックアップファイルを消去

--- go/.backup20231113/scripts/reset_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-migrate -database 'mysql://root:rootpassword@tcp(mysql)/mydatabase?query' -path migrations drop -f
-migrate -database 'mysql://root:rootpassword@tcp(mysql)/mydatabase?query' -path migrations up
-go run cmd/seed/main.go
