b6233ad8e0eab5562c5892cf72c69d4a0041a662
komu003 <komu.ph342@gmail.com>
2023-11-19 16:09:03 +0900

    add: APIUsersGetハンドラ

--- go/services/user_service.go
+++ go/services/user_service.go
@@ -10,6 +10,27 @@ import (
 
 type UserService struct{}
 
+func (s *UserService) APIUsersGet(ctx context.Context) (ogen.APIUsersGetRes, error) {
+	var users []models.User
+	if err := db.DB.WithContext(ctx).Find(&users).Error; err != nil {
+		errMsg := fmt.Errorf("データベースからのユーザー一覧の取得に失敗しました: %w", err)
+		return &ogen.APIUsersGetInternalServerError{}, errMsg
+	}
+
+	apiUsers := make(ogen.APIUsersGetOKApplicationJSON, len(users))
+	for i, user := range users {
+		apiUsers[i] = ogen.User{
+			ID:        ogen.NewOptInt(int(user.ID)),
+			Name:      ogen.NewOptString(user.Name),
+			Email:     ogen.NewOptString(user.Email),
+			CreatedAt: ogen.NewOptDateTime(user.CreatedAt),
+			UpdatedAt: ogen.NewOptDateTime(user.UpdatedAt),
+		}
+	}
+
+	return &apiUsers, nil
+}
+
 func (s *UserService) APIUsersCountGet(ctx context.Context) (ogen.APIUsersCountGetRes, error) {
 	var count int64
 	if err := db.DB.WithContext(ctx).Model(&models.User{}).Count(&count).Error; err != nil {
