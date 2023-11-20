49b38ec724529d4820ee83433010b77ec93575ce
komu003 <komu.ph342@gmail.com>
2023-11-20 17:40:34 +0900

    add: 実装すべきテストを必ず成功するテストとして追加

---
+++ go/integration_tests/micropost_service_test.go
@@ -0,0 +1,14 @@
+package integration_tests
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAPIMicropostsGetIntegration(t *testing.T) {
+	assert.True(t, true, "必ず成功するテスト")
+}
+
+func TestAPIMicropostsCountGetIntegration(t *testing.T) {
+	assert.True(t, true, "必ず成功するテスト")
+}
---
+++ go/integration_tests/user_service_test.go
@@ -0,0 +1,14 @@
+package integration_tests
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAPIUsersGetIntegration(t *testing.T) {
+	assert.True(t, true, "必ず成功するテスト")
+}
+
+func TestAPIUsersCountGetIntegration(t *testing.T) {
+	assert.True(t, true, "必ず成功するテスト")
+}
