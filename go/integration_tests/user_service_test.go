30a32219c5d3db33919274e0f8478e0ba6895718
komu003 <komu.ph342@gmail.com>
2023-11-21 17:52:39 +0900

    add: TestAPIUsersCountGetIntegrationに0件のテストケース追加

--- go/integration_tests/user_service_test.go
+++ go/integration_tests/user_service_test.go
@@ -16,23 +16,45 @@ func TestAPIUsersGetIntegration(t *testing.T) {
 }
 
 func TestAPIUsersCountGetIntegration(t *testing.T) {
-	testUser := &models.User{Name: "テストユーザー1", Email: "test1@example.com"}
-	err := db.Create(testUser).Error
-	require.NoError(t, err, "テストデータの挿入に失敗しました")
-	defer db.Delete(testUser)
-
-	url := fmt.Sprintf("%s/api/users/count", testHTTP.URL)
-	res, err := http.Get(url)
-	require.NoError(t, err, "HTTPリクエストの送信に失敗しました")
-	defer res.Body.Close()
-
-	var countResponse struct {
-		Count int64 `json:"count"`
+	cases := []struct {
+		name     string
+		setup    func() error
+		expected int64
+	}{
+		{
+			name: "ゼロユーザー",
+			setup: func() error {
+				return nil
+			},
+			expected: 0,
+		},
+		{
+			name: "1ユーザー",
+			setup: func() error {
+				return db.Create(&models.User{Name: "テストユーザー1", Email: "test1@example.com"}).Error
+			},
+			expected: 1,
+		},
 	}
 
-	err = json.NewDecoder(res.Body).Decode(&countResponse)
-	require.NoError(t, err, "レスポンスのデコードに失敗しました")
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			require.NoError(t, tc.setup(), "テストデータのセットアップに失敗しました")
 
-	assert.Equal(t, http.StatusOK, res.StatusCode, "HTTPステータスコードが期待と異なります")
-	assert.Equal(t, int64(1), countResponse.Count, "ユーザー数が期待と異なります")
+			url := fmt.Sprintf("%s/api/users/count", testHTTP.URL)
+			res, err := http.Get(url)
+			require.NoError(t, err, "HTTPリクエストの送信に失敗しました")
+			defer res.Body.Close()
+
+			var countResponse struct {
+				Count int64 `json:"count"`
+			}
+
+			err = json.NewDecoder(res.Body).Decode(&countResponse)
+			require.NoError(t, err, "レスポンスのデコードに失敗しました")
+
+			assert.Equal(t, http.StatusOK, res.StatusCode, "HTTPステータスコードが期待と異なります")
+			assert.Equal(t, tc.expected, countResponse.Count, "ユーザー数が期待と異なります")
+		})
+	}
 }
